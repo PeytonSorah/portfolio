997f78cdff65babd51b658bc3b7a04437bd6beba
PeytonSorah <peytonesorah@gmail.com>
2016-03-28 23:39:24 -0400

    front page edits

--- page-home.php
+++ page-home.php
@@ -19,12 +19,7 @@
 
 				<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
+						<main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 								<section class="entry-content cf" itemprop="articleBody">
 									   <?php
@@ -37,6 +32,7 @@
 											      $caption = get_sub_field( 'caption' ); ?>
 
 														<div class="slide" style="background-image:url('<?php echo $image[url]; ?>');">
+															<div class="caption"> <h3><?php echo $caption; ?></h3> </div>
 											      </div>
 
 											    <?php endwhile; ?>
@@ -47,23 +43,43 @@
 
 								</section>
 
-							</article>
+								<section class="home-news">
 
-							<?php endwhile; else : ?>
+						      <div class="home-news-wrap">
+						        <?php 
+						          $args = array(
+						          'numberposts' => 2,
+						          'posts_per_page' => 2,
+						          'orderby' => 'post_date',
+						          'order' => 'DESC',
+						          'post_type' => 'portfolio',
+						          );
+						          $the_query = new WP_Query( $args );
+						          if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						          <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' ); ?>
 
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+						        <div class="home-news-item">
+						          <a href="<?php the_permalink() ?>">
+							          <div class="squarephoto" style="background: url(<?php echo $src[0]; ?> ) !important;"></div>			            
+							          <section class="home-news-item-text">
+						          		<h2 class="white-ribbon"><?php the_date('m / d'); ?> </h2>
+							            <h3><?php echo get_the_title(); ?></h3>
+							            <p><?php the_excerpt(); ?></p>
+							          </section>
+						          </a>
+						        </div>
 
-							<?php endif; ?>
+						        <?php endwhile; ?>
+						        <?php else : ?>
+						        <?php get_template_part( 'library/post-not-found' ); ?>
+						        <?php endif; ?>
+
+						        <a href="<?php echo esc_url( home_url() ); ?>/portfolio">
+						        	see more work
+						        </a>
+
+						    </div>
+						  </section>
 
 						</main>
 
